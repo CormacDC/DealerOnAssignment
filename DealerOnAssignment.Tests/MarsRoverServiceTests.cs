5a269e8ef743b22c3fb6fd34cf6a968e51e03a13
CormacDC <60583567+CormacDC@users.noreply.github.com>
2023-02-08 00:14:43 -0500

    add tests for GetFinalBearing

--- DealerOnAssignment.Tests/MarsRoverServiceTests.cs
+++ DealerOnAssignment.Tests/MarsRoverServiceTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Numerics;
+using Xunit.Sdk;
 
 namespace DealerOnAssignment.Tests;
 
@@ -22,7 +24,8 @@ public class MarsRoverServiceTests
     [InlineData("1000 1000", 1000, 1000)]
     [InlineData("0 0", 0, 0)]
     public void ConvertGridSizeToVector_ValidInput_CorrectVectorOut(
-        string gridSize, float expectedX, float expectedY)
+        string gridSize,
+        float expectedX, float expectedY)
     {
         var gridVector = _MarsRoverService.ConvertGridSizeToVector(gridSize);
 
@@ -35,13 +38,33 @@ public class MarsRoverServiceTests
     [InlineData("1 6 E", 1, 6, 5)]
     [InlineData("1000 1000 S", 1000, 1000, 6)]
     [InlineData("0 0 W", 0, 0, 7)]
-    public void ConvertStringBearingToVector_ValidNorthInput_CorrectVectorOut(
-        string bearing, float expectedX, float expectedY, float expectedZ)
+    public void ConvertStringBearingToVector_ValidInput_CorrectVectorOut(
+        string bearing,
+        float expectedX, float expectedY, float expectedZ)
     {
         var bearingVector = _MarsRoverService.ConvertStringBearingToVector(bearing);
 
         Assert.True(Math.Abs(bearingVector.X - expectedX) < 0.001);
         Assert.True(Math.Abs(bearingVector.Y - expectedY) < 0.001);
         Assert.True(Math.Abs(bearingVector.Z - expectedZ) < 0.001);
+    }
+
+    [Theory]
+    [InlineData(new[] { (float)5, 5 }, new[] { (float)1, 2, 4 }, "LMLMLMLMM", new[] { (float)1, 3, 4 })]
+    [InlineData(new[] { (float)5, 5 }, new[] { (float)3, 3, 5 }, "MMRMMRMRRM", new[] { (float)5, 1, 5 })]
+    [InlineData(new[] { (float)5, 5 }, new[] { (float)3, 3, 5 }, "MMRMMRMRRMMMM", new[] { (float)5, 1, 5 })]
+    public void GetFinalBearing_ValidInput_CorrectVectorOut(
+        float[] gridSize, float[] initialBearing, string directions, 
+        float[] expectedBearing)
+    {
+        var gridSizeVector = new Vector2(gridSize[0], gridSize[1]);
+        var initialBearingVector = new Vector3(initialBearing[0], initialBearing[1], initialBearing[2]);
+        
+        var finalBearingVector = 
+            _MarsRoverService.GetFinalBearing(gridSizeVector, initialBearingVector, directions);
+
+        Assert.True(finalBearingVector.X - expectedBearing[0] < 0.001
+                    && finalBearingVector.Y - expectedBearing[1] < 0.001
+                    && finalBearingVector.Z - expectedBearing[2] < 0.001);
     }
 }
